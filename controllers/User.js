90e62aef0a786aed1f55775d9a2031b2554618e3
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-11 22:13:56 +0700

    user login

--- controllers/User.js
+++ controllers/User.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const hashedPassword = (password, salt) => {
@@ -19,5 +20,36 @@ exports.register = async (req, res, next) => {
   res.status(201).json({
     status: "success",
     message: "User has been created!!",
+  });
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new Error("Please provide email and password");
+  }
+
+  const target = await User.findOne({
+    email,
+  }).select("+password");
+
+  if (!target || !(await target.correctPassword(password, target.password))) {
+    throw new Error("Incorrect email or password");
+  }
+
+  const payload = {
+    id: target._id,
+    email: target.email,
+    name: target.name,
+  };
+
+  const token = jwt.sign(payload, "this-is-secret", {
+    expiresIn: "7d",
+  });
+
+  res.status(200).json({
+    message: "Login successfully!!",
+    token,
   });
 };
