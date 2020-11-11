f78b45822a1b4838d4f1249d0047b61f14a26356
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-11 22:27:32 +0700

    body-parser

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.static(`${__dirname}/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extends: false }));
 
 module.exports = app;
